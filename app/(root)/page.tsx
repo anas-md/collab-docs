b4a18fec00df0365d6b64718969e334346eb451a
Anas <anasdaud1996@gmail.com>
2024-07-22 07:28:27 +0800

    Add Editor Component

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -3,7 +3,7 @@ import { Button } from '@/components/ui/button';
 const Home = () => {
   return (
     <div>
-      <Button>Initial Commit</Button>
+      <Button>Submit</Button>
     </div>
   );
 };
