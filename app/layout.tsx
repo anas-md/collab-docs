e9246d9ed3a0b09fd92814faf6fd0e539cdf8d1c
Anas <anasdaud1996@gmail.com>
2024-07-24 11:11:58 +0800

    Add Liveblocks Collaboration Components

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { Inter as FontSans } from 'next/font/google';
 import { cn } from '@/lib/utils';
 import { ClerkProvider } from '@clerk/nextjs';
 import { dark } from '@clerk/themes';
+import Provider from './Provider';
 
 const fontSans = FontSans({
   subsets: ['latin'],
@@ -34,7 +35,7 @@ export default function RootLayout({
             fontSans.variable
           )}
         >
-          {children}
+          <Provider>{children}</Provider>
         </body>
       </html>
     </ClerkProvider>
